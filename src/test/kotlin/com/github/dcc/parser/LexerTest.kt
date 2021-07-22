1c29a219fad045bbac1aded4a9d463367b9ed902
Josue Valenzuela <val171001@uvg.edu.gt>
2021-07-21 18:17:24 -0600

    Add lexer test

---
+++ src/test/kotlin/com/github/dcc/parser/LexerTest.kt
@@ -0,0 +1,44 @@
+package com.github.dcc.parser
+
+import com.github.dcc.tokens
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class LexerTest {
+
+    @Test
+    fun `should get all tokens from decaf source`() {
+        val source = """class Program { char var; int main() { var = 'A'; return 0; } }"""
+        val charStream = ANTLRInputStream(source.toCharArray(), source.length)
+        val lexer = DecafLexer(charStream)
+
+        val expectedTokens = listOf(
+            "CLASS",
+            "PROGRAM",
+            "OCURLY",
+            "CHAR",
+            "ID",
+            "SEMICOLON",
+            "INT",
+            "ID",
+            "OPARENTHESIS",
+            "CPARENTHESIS",
+            "OCURLY",
+            "ID",
+            "EQ",
+            "CHAR_LITERAL",
+            "SEMICOLON",
+            "RETURN",
+            "NUM",
+            "SEMICOLON",
+            "CCURLY",
+            "CCURLY"
+        )
+
+        val tokens  = lexer.tokens()
+
+        assertEquals(expectedTokens, tokens)
+    }
+
+}
---
+++ src/test/kotlin/com/github/dcc/utils.kt
@@ -0,0 +1,21 @@
+package com.github.dcc
+
+import com.github.dcc.parser.DecafLexer
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.CommonTokenStream
+
+const val EOF = "EOF"
+
+fun Any.getResourceAsStream(file: String) = this::class.java.getResourceAsStream(file)
+
+fun Any.getResourceAsANTLRInputStream(file: String) = ANTLRInputStream(getResourceAsStream(file))
+
+fun Any.getResourceAsDecafLexer(file: String) = DecafLexer(getResourceAsANTLRInputStream(file))
+
+fun Any.getResourceAsDecafTokenStream(file: String) = CommonTokenStream(getResourceAsDecafLexer(file))
+
+fun DecafLexer.tokens(): List<String> = allTokens.map {
+    if(it.type == -1)
+        EOF
+    else vocabulary.getSymbolicName(it.type)
+}
