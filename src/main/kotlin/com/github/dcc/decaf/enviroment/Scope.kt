be01f6d2e4276d6f8c55c33d5bf3c89dcc2676eb
Josue Valenzuela <val171001@uvg.edu.gt>
2021-08-04 17:53:46 -0600

    Add scope base implementation

--- src/main/kotlin/com/github/dcc/decaf/enviroment/Scope.kt
+++ src/main/kotlin/com/github/dcc/decaf/enviroment/Scope.kt
@@ -3,10 +3,44 @@ package com.github.dcc.decaf.enviroment
 /*
  Supported envs defined in decaf spec
 */
-sealed class Scope {
-    object Global : Scope()
+sealed class Scope(
+    open val parent: Scope
+) {
+    object Global : Scope(Global) {
+        override fun toString(): String = this::class.simpleName ?: super.toString()
+    }
 
     data class Local(
-        val id: String //TODO: Define how to identify local scopes
-    ): Scope()
+        val name: String, //method name
+        override val parent: Scope = Global
+    ) : Scope(parent) {
+        override fun toString(): String = name
+    }
+
+    val parents: List<Scope>
+        get() {
+            val acc = mutableListOf<Scope>()
+            var next = parent
+            while (next != Global) {
+                acc.add(next)
+                next = next.parent
+            }
+            return acc
+        }
+
+    fun genId(): String {
+
+       return when(this) {
+            is Global -> Global.toString()
+            is Local -> {
+                parents.toMutableList().apply {
+                    reverse()
+                    add(this@Scope)
+                }.joinToString(separator = "@") { it.toString() }
+            }
+        }
+    }
+
+    fun child(name: String) = Local(name, this)
+
 }
--- src/main/kotlin/com/github/dcc/decaf/symbols/Symbol.kt
+++ src/main/kotlin/com/github/dcc/decaf/symbols/Symbol.kt
@@ -1,6 +1,5 @@
 package com.github.dcc.decaf.symbols
 
-import com.github.dcc.decaf.DecafElement
 import com.github.dcc.decaf.enviroment.Scope
 import com.github.dcc.decaf.types.Type
 
@@ -8,26 +7,22 @@ import com.github.dcc.decaf.types.Type
  Supported symbols by decaf spec
 */
 sealed class Symbol(
-    val name: String,
-    val scope: Scope,
-    val type: Type
-) : DecafElement() {
+    open val name: String,
+    open val scope: Scope,
+    open val type: Type
+) {
 
-    class Variable(
-        name: String,
-        scope: Scope,
-        type: Type,
+    data class Variable(
+        override val name: String,
+        override val scope: Scope,
+        override val type: Type,
     ) : Symbol(name, scope, type)
 
-    class Method(
-        name: String,
-        scope: Scope,
-        type: Type,
+    data class Method(
+        override val name: String,
+        override val scope: Scope,
+        override val type: Type,
         val signature: String //TODO: Define signature
     ) : Symbol(name, scope, type)
-
-    override fun toString(): String {
-        return "Symbol(name=$name, type=$type)"
-    }
 
 }
