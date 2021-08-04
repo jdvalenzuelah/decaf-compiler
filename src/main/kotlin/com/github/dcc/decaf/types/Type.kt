a7bcdf514a7894e205e1838f6b5f26d4e1413f0b
Josue Valenzuela <val171001@uvg.edu.gt>
2021-08-04 17:53:19 -0600

    Add missing types for unknown size array

--- src/main/kotlin/com/github/dcc/decaf/types/Type.kt
+++ src/main/kotlin/com/github/dcc/decaf/types/Type.kt
@@ -1,12 +1,13 @@
 package com.github.dcc.decaf.types
 
-import com.github.dcc.decaf.DecafElement
 import com.github.dcc.decaf.symbols.Symbol
 
 /*
  Types supported by decaf spec
 */
-sealed class Type : DecafElement() {
+sealed class Type {
+
+    override fun toString(): String = this::class.simpleName ?: super.toString()
 
     object Any : Type() // wildcard
 
@@ -33,6 +34,12 @@ sealed class Type : DecafElement() {
         val size: kotlin.Int,
         val type: Type
     ): Type() {
+        init { require(type !is StructDecl) }
+    }
+
+    data class ArrayUnknownSize(
+        val type: Type,
+    ) : Type() {
         init { require(type !is StructDecl) }
     }
 }
