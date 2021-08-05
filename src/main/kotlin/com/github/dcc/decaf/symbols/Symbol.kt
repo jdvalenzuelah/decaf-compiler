bcee302d1e6212a9a2dc81a5ccca8d829ce913bb
Josue Valenzuela <val171001@uvg.edu.gt>
2021-08-04 21:35:25 -0600

    Add uuid to symbol

--- src/main/kotlin/com/github/dcc/decaf/symbols/Symbol.kt
+++ src/main/kotlin/com/github/dcc/decaf/symbols/Symbol.kt
@@ -2,6 +2,7 @@ package com.github.dcc.decaf.symbols
 
 import com.github.dcc.decaf.enviroment.Scope
 import com.github.dcc.decaf.types.Type
+import java.util.*
 
 /*
  Supported symbols by decaf spec
@@ -11,6 +12,8 @@ sealed class Symbol(
     open val scope: Scope,
     open val type: Type
 ) {
+
+    val id by lazy { UUID.randomUUID().toString() }
 
     data class Variable(
         override val name: String,
