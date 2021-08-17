52b248156382c207d6a23938388d768020f0fc80
Josue Valenzuela <val171001@uvg.edu.gt>
2021-08-16 19:20:11 -0600

    Reduce exposed api to specific compiling context

--- libdcc/src/main/kotlin/com/github/dcc/compiler/CompilerContext.kt
+++ libdcc/src/main/kotlin/com/github/dcc/compiler/CompilerContext.kt
@@ -1,13 +1,29 @@
 package com.github.dcc.compiler
 
+import com.github.dcc.parser.DecafLexer
 import com.github.dcc.parser.DecafParser
+import org.antlr.v4.runtime.ANTLRInputStream
 import org.antlr.v4.runtime.BufferedTokenStream
+import java.io.File
 
-data class CompilerContext(
+class CompilerContext(
     val tokenStream: BufferedTokenStream,
 ) {
+
+    constructor(file: File) : this(
+        BufferedTokenStream(
+            DecafLexer(
+                ANTLRInputStream(
+                    file.inputStream()
+                )
+            )
+        )
+    )
+
     val parser: DecafParser get() {
         tokenStream.reset()
         return DecafParser(tokenStream)
     }
+
+    fun reset(): Unit = tokenStream.reset()
 }
