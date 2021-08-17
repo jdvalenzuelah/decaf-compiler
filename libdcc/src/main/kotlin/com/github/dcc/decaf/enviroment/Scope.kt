c4ebfcaaaea092c2914f99569a72e1149b372bf6
Josue Valenzuela <val171001@uvg.edu.gt>
2021-08-16 19:19:26 -0600

    Add dcc elements to string methods

--- libdcc/src/main/kotlin/com/github/dcc/decaf/enviroment/Scope.kt
+++ libdcc/src/main/kotlin/com/github/dcc/decaf/enviroment/Scope.kt
@@ -1,16 +1,31 @@
 package com.github.dcc.decaf.enviroment
 
+import java.util.*
+
 sealed class Scope(
     open val parent: Scope
 ) {
-    object Global : Scope(Global)
+    object Global : Scope(Global) {
+        override fun toString(): String = this::class.simpleName ?: super.toString()
+    }
 
     data class Local(
         val name: String,
         override val parent: Scope = Global
-    ): Scope(parent)
+    ): Scope(parent) {
+        override fun toString(): String = name
+    }
 
 }
 
+fun Scope.lineage(): LinkedList<Scope> =
+    if(this == Scope.Global)
+        LinkedList<Scope>().apply { addFirst(this@lineage) }
+    else {
+        parent.lineage()
+            .apply { addLast(this@lineage) }
+    }
+
+fun Scope.lineageAsString(): String = this.lineage().joinToString(separator = "@") { it.toString() }
 
 fun Scope.child(name: String): Scope = Scope.Local(name, this)
--- libdcc/src/main/kotlin/com/github/dcc/decaf/types/Type.kt
+++ libdcc/src/main/kotlin/com/github/dcc/decaf/types/Type.kt
@@ -11,18 +11,25 @@ sealed class Type {
 
     data class Struct(
         val name: String,
-    ): Type()
+    ): Type() {
+        override fun toString(): String = name
+    }
 
     data class Array(
         val size: kotlin.Int,
         val type: Type,
         val context: ParserRuleContext,
     ): Type() {
+        override fun toString(): String = "${type}Array[$size]"
     }
 
     data class ArrayUnknownSize(
         val type: Type,
         val context: ParserRuleContext,
-    ): Type()
+    ): Type() {
+        override fun toString(): String = "${type}Array[]"
+    }
+
+    override fun toString(): String = this::class.simpleName ?: super.toString()
 
 }
