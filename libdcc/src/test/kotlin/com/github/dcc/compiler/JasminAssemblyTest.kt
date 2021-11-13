ff56bdaa8c3daf1f33b24232c1ebecbc22dd6653
Josue Valenzuela <val171001@uvg.edu.gt>
2021-11-12 20:54:43 -0600

    Add interactive mode to jasmin tests

--- libdcc/src/test/kotlin/com/github/dcc/compiler/JasminAssemblyTest.kt
+++ libdcc/src/test/kotlin/com/github/dcc/compiler/JasminAssemblyTest.kt
@@ -18,26 +18,33 @@ class JasminAssemblyTest {
         }
     }
 
-    private fun execProgram(input: Iterable<Any> = emptyList()): String {
+    private fun execProgram(input: Iterable<Any> = emptyList(), interactive: Boolean = false): String {
         val process = Runtime.getRuntime().exec("java -noverify -cp ${generatedFilesFolder.path} Program")
 
         val processOutput = BufferedReader(InputStreamReader(process.inputStream))
         val processInput = BufferedWriter(OutputStreamWriter(process.outputStream))
 
+        val feedback = StringBuilder()
+
         processInput.use { writer ->
             input.forEach {
                 writer.appendLine(it.toString())
                 writer.flush()
-                processOutput.readLine()
+                feedback.appendLine(processOutput.readLine())
             }
         }
 
         process.waitFor()
 
-        return processOutput.readText()
+        process.outputStream
+
+        return if(interactive) feedback
+            .append(processOutput.readText())
+            .toString()
+        else processOutput.readText()
     }
 
-    private fun assertSuccessAndExpectedOutput(sourceFile: File, expected: String, programInput: Iterable<Any> = emptyList()) {
+    private fun assertSuccessAndExpectedOutput(sourceFile: File, expected: String, programInput: Iterable<Any> = emptyList(), interactive: Boolean = false) {
         val compiler = Compiler(sourceFile, Target.JASMIN)
 
         val compilationResult = compiler.compileSource()
@@ -47,7 +54,7 @@ class JasminAssemblyTest {
         val compiledSource = (compilationResult as Compiler.CompilationResult.Success).compiledSource
 
         assemble(compiledSource)
-        val executionResult = execProgram(programInput)
+        val executionResult = execProgram(programInput, interactive)
 
         assertEquals(expected, executionResult.trim())
     }
@@ -62,6 +69,8 @@ class JasminAssemblyTest {
         assertSuccessAndExpectedOutput(getFileFromResources("/samples/quicksort.decaf"), "1\n2\n3\n4\n5\n6\n7\n8\n9\n10", 10 downTo 1)
         assertSuccessAndExpectedOutput(getFileFromResources("/samples/scope.decaf"), "3\n5\n6\n7\n8\n5\n4")
         assertSuccessAndExpectedOutput(getFileFromResources("/samples/structs.decaf"), "2")
+        assertSuccessAndExpectedOutput(getFileFromResources("/samples/multiple_tests.decaf"), "1\n1\n2\n6\n24\n6\n6\n720\n40320\n362880\n3628800\n39916800", 0..11, true)
+
     }
 
 }
